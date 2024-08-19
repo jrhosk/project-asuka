bcbf0eaefcfa8e31392075da7dc22d80e657e512
Joshua Hoskins <jrh4dy@gmail.com>
2024-08-19 14:19:39 -0400

    Add security code.

--- graph.py
+++ graph.py
@@ -1,6 +1,5 @@
+import os
 import rich
-import scp
-import paramiko
 import requests
 import pathlib
 import configparser
@@ -11,31 +10,50 @@ from rich.markup import escape
 from rich.text import Text
 from rich.tree import Tree
 
+from graphviper.utils import logger
+
 
 class GraphQuery:
-    def __init__(self, config=None):
-        if not pathlib.Path(config).exists():
+    def __init__(self):
+        config_file = "/".join((str(pathlib.Path(__file__).parent.resolve()), ".graph/config.cfg"))
+
+        logger.info(f"{config_file}")
+
+        if not pathlib.Path(config_file).exists():
             print("Missing configuration file.")
 
-        settings = configparser.ConfigParser()
+        config = configparser.ConfigParser()
+        config.read(config_file)
 
-        self.settings = settings.read(config)
-        self.hostname = settings.get("msgraph", "hostname")
-        self.app_token = settings.get("msgraph", "app_token")
-        self.version = settings.get("msgraph", "version")
+        self.app_token = config.get("msgraph", "app_token")
+        self.hostname = config.get("msgraph", "hostname")
+        self.version = config.get("msgraph", "version")
 
-        self.response = None
+        if os.getenv("APP_TOKEN"):
+            # Could add some verification that the token is correct here
+            self.app_token = os.getenv("APP_TOKEN")
 
-    def listdir(self, path: str)->None:
-        header = {
+        if self.app_token == "None":
+            from security.encryption import get_credentials
+
+            logger.info("Attempting to get credentials ...")
+            self.app_token = get_credentials(persistent=True)
+            # Could add some verification that the token is correct here
+
+        self.header = {
             "Host": "graph.microsoft.com",
             "Authorization": f"Bearer {self.app_token}",
             "Content-Type": "application/json"
         }
 
+        self.drive = None
+        self.response = None
+
+    def listdir(self, path: str) -> None:
+
         self.response = requests.get(
             url=f"https://{self.hostname}/{self.version}/me/drive/root:/{path}:/children",
-            headers=header
+            headers=self.header
         )
 
         tree = Tree(
@@ -54,12 +72,14 @@ class GraphQuery:
 
             else:
                 text_filename = Text(entry["name"], "green")
+
                 #text_filename.highlight_regex(r"\..*$", "bold red")
                 text_filename.stylize(f" link file://{entry['parentReference']['path']}")
+
                 #file_size = path.stat().st_size
                 #text_filename.append(f" ({decimal(file_size)})", "blue")
                 icon = "📦 " if entry['name'].rsplit(".")[-1] == "zip" else "📄 "
-                print(entry['name'].rsplit(".")[-1])
+
                 #icon = "📄 "
                 #icon = "📦 "
                 tree.add(Text(icon) + text_filename)
